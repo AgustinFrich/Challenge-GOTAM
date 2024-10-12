7b0169c0d2a5698de573a12e0cf755e85f70e297
AgustinFrich <agustinfrich@gmail.com>
2024-10-12 16:53:21 -0300

    App module change

--- challenge-gotam-server/src/app.module.ts
+++ challenge-gotam-server/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { EmpleadoModule } from './empleado/empleado.module';
-import { AreaModule } from './area/area.module';
+import { EmpleadoModule } from 'src/empleado/empleado.module';
+import { AreaModule } from 'src/area/area.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), EmpleadoModule, AreaModule],
